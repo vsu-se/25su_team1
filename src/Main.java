24299546865d6d022de8f9e969405da77546da29
Hola-Chrisotk <chrisramos811@gmail.com>
2025-07-11 16:12:22 -0400

    Added Manager and Staff Class
    Added Manager and Staff Classes

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         String firstName, lastName, position;
         Employee a = null;
+        Manager aa = null;
         System.out.println("New Employee? y/n");
         Scanner input = new Scanner(System.in);
         String answer = input.nextLine();
@@ -13,14 +14,26 @@ public class Main {
             lastName = input.nextLine();
             System.out.println("position? (Manager or Staff)");
             position = input.nextLine();
-            a = new Employee(firstName + " " + lastName, position);
+            switch (position) {
+                case "Manager":
+                    aa = new Manager(firstName + " "+ lastName);
+                    break;
+                case "Staff":
+                    a = new Employee(firstName + " " + lastName, position);
+                default:
+                    System.out.println("Invalid Answer");
+                    break;
+            }
         }
 
         if (a != null) {
             System.out.println("Employee Name: " + a.getName());
             System.out.println("Position: " + a.getPosition());
+        } else if (aa != null)  {
+            System.out.println("Employee Name: " + aa.getName());
+            System.out.println("Position: " + aa.getPosition());
         } else {
-            System.out.println("No employee was created.");
+            System.out.println("No employeees");
         }
 
         input.close(); // Good practice to close Scanner
---
+++ src/Manager.java
@@ -0,0 +1,7 @@
+public class Manager extends Employee {
+
+    public Manager(String name) {
+        super(name, "Manager"); // Call Employee constructor
+    }
+    
+}
