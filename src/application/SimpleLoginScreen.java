b64a0de448c7b3039e496aa2728e644d095bb820
Hola-Chrisotk <chrisramos811@gmail.com>
2025-07-11 22:29:55 -0400

    Added @pearsonjason 's gui
    Added the GUI so i could implement adding employees.

---
+++ src/application/SimpleLoginScreen.java
@@ -0,0 +1,109 @@
+package application;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SimpleLoginScreen extends Application {
+	
+	Stage window;
+	Scene scene1, scene2, scene3;
+
+    private Map<String, String> user = new HashMap<>();
+
+    @Override
+    public void start(Stage primaryStage) {
+    	window = primaryStage;
+        initializeUsers();
+
+        Label userLabel = new Label("Username:");
+        TextField userField = new TextField();
+
+        Label passLabel = new Label("Password:");
+        PasswordField passField = new PasswordField();
+
+        Button loginButton = new Button("Login");
+        Label messageLabel = new Label();
+
+        
+        
+        loginButton.setOnAction(e -> {
+            String username = userField.getText();
+            String password = passField.getText();
+
+            if (user.containsKey(username) && user.get(username).equals(password)) {
+                messageLabel.setText("Login successful!");
+                window.setScene(scene2);
+                
+            } else {
+                messageLabel.setText("Invalid username or password.");
+            }
+        });
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(userLabel, userField, passLabel, passField, loginButton, messageLabel);
+        scene1 = new Scene(layout, 400, 300);
+        window.setScene(scene1);
+        window.setTitle("Manager Login");
+        window.show();
+        
+        
+        //Layout 2 Will be used later on for delete or what now.
+        Button addEmployeeScene = new Button("Add employee");
+        addEmployeeScene.setOnAction(e -> window.setScene(scene3));
+        VBox layout2 = new VBox(10);
+        layout2.getChildren().add(addEmployeeScene);
+        scene2 = new Scene(layout2, 600,300);
+  
+        
+        
+        //Layout 3
+        CreateEmployee creator = new CreateEmployee();
+        Label firstName = new Label("Enter First Name");
+        TextField firstNamefield = new TextField();
+
+        Label lastName = new Label("Enter Last name:");
+        TextField lastNamefield = new TextField();
+
+        ComboBox<String> position = new ComboBox<>();
+        position.getItems().addAll("Manager", "Staff");
+        position.setValue("Manager");
+        Button addEmployee = new Button("Add Employee");
+        
+        addEmployee.setOnAction(e -> {
+        	String firstname = firstNamefield.getText();
+        	String lastname = lastNamefield.getText();
+        	String Position = position.getValue();
+        	if(firstname.length() > 0 && lastname.length() > 0) {
+        		creator.yo(firstname, lastname, Position);
+        		messageLabel.setText("added employee successfully!");
+                window.setScene(scene1);
+        	} else {
+                messageLabel.setText("Failed to add employee.");
+            }
+        	
+        	
+        });
+        
+        VBox layout3 = new VBox(10);
+        layout3.getChildren().addAll(firstName, firstNamefield, lastName, lastNamefield, position, addEmployee);
+        scene3 = new Scene(layout3, 600, 300);
+        
+       
+        
+        
+    }
+
+    private void initializeUsers() {
+        user.put("jason", "password234");
+        user.put("manager1", "letmein");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
