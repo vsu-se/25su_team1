9276f8ce71f8a4557aacd99f231cdb8e9b4b2c49
Jason <112111941+pearsonjason@users.noreply.github.com>
2025-07-30 18:19:04 -0400

    Delete src directory

--- src/application/Manager.java
+++
@@ -1,9 +0,0 @@
-package application;
-
-public class Manager extends Employee {
-
-    public Manager(String FirstName, String LastName, String UserName, String Password) {
-        super(FirstName, LastName, UserName, Password, "Manager"); // Call Employee constructor
-    }
-    
-}
--- src/module-info.java
+++
@@ -1,5 +0,0 @@
-module Simplegui {
-	requires javafx.controls;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
